45959ebd215d80143df5c6056af9bf959d305d30
Anand Sahu <anandsahu5097@gmail.com>
2024-12-07 12:38:17 +0530

    project structure ready,venv setup completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anand Sahu",
+    author_email="anandsahu5097@gmail.com",
+    packages=find_packages()
+)
